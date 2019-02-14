6215a4cb9d60e9993a9e863184235cbd9614bcee
Dan Fu <danfu09@gmail.com>
2019-02-14 22:02:03 +0000

    Change file saving format

--- utils/video_jpg.py
+++ utils/video_jpg.py
@@ -12,12 +12,12 @@ if __name__=="__main__":
     if '.mp4' not in file_name:
       continue
     name, ext = os.path.splitext(file_name)
-    dst_directory_path = os.path.join(dst_dir_path, name)
+    dst_directory_path = os.path.join(dst_dir_path, file_name)
 
     video_file_path = os.path.join(dir_path, file_name)
     try:
       if os.path.exists(dst_directory_path):
-        if not os.path.exists(os.path.join(dst_directory_path, 'image_00001.jpg')):
+        if not os.path.exists(os.path.join(dst_directory_path, 'image_1.jpg')):
           subprocess.call('rm -r {}'.format(dst_directory_path), shell=True)
           print('remove {}'.format(dst_directory_path))
           os.mkdir(dst_directory_path)
@@ -28,7 +28,7 @@ if __name__=="__main__":
     except:
       print(dst_directory_path)
       continue
-    cmd = 'ffmpeg -i {} -vf scale=-1:360 {}/image_%05d.jpg'.format(video_file_path, dst_directory_path)
+    cmd = 'ffmpeg -i {} -vf scale=-1:360 {}/image_%d.jpg'.format(video_file_path, dst_directory_path)
     print(cmd)
     subprocess.call(cmd, shell=True)
     print('\n')
